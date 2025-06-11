37aa69299dfa5629e690aeb392c101def282786d
Luiz-Carlos-Official <luizcarlosofficial46@gmail.com>
2025-06-11 10:20:25 -0400

    crie a pasta exe001 e os exe01 e exe02

---
+++ exercicios/exe001/exe02.py
@@ -0,0 +1,6 @@
+nome = input ('Qual seu nome: ')
+print (f'Olá {nome}')
+n1 = int(input ('Primeiro número: '))
+n2 = int(input ('Segundo número: '))
+s = n1 + n2
+print (f'{nome}, a soma de: {n1} + {n2} = {s}')
